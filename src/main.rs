627fc738bf6d1496ca65fb200a3d2a11594172f5
luoxiao <luoxiaozero@163.com>
2022-07-21 12:29:22 +0800

    feat: 完善样例

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,23 @@ use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Equal => println!("Too big!"),
-        std::cmp::Ordering::Greater => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Equal => {
+                println!("Too big!");
+                break;
+            }
+            std::cmp::Ordering::Greater => println!("You win!"),
+        }
     }
-
-    println!("{} {}", guess, secret_number);
 }
